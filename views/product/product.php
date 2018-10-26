e233805cd6cbee34ea57906eb41f1ed874bb4cd2
taras824 <taras824ss@gmail.com>
2018-10-26 19:41:35 +0300

    Remove Empty Lines
    remove duplicate HTML code

--- views/product/product.php
+++ views/product/product.php
@@ -125,45 +125,6 @@
 					</div>
 					<?php endforeach; ?>
 
-					<!-- Product -->
-					<!-- <div class="product">
-						<div class="product_image"><img src="/template/images/product_1.jpg" alt=""></div>
-						<div class="product_extra product_new"><a href="categories.html">New</a></div>
-						<div class="product_content">
-							<div class="product_title"><a href="product.html">Smart Phone</a></div>
-							<div class="product_price">$670</div>
-						</div>
-					</div> -->
-
-					<!-- Product -->
-					<!-- <div class="product">
-						<div class="product_image"><img src="/template/images/product_2.jpg" alt=""></div>
-						<div class="product_extra product_sale"><a href="categories.html">Sale</a></div>
-						<div class="product_content">
-							<div class="product_title"><a href="product.html">Smart Phone</a></div>
-							<div class="product_price">$520</div>
-						</div>
-					</div> -->
-
-					<!-- Product -->
-					<!-- <div class="product">
-						<div class="product_image"><img src="/template/images/product_3.jpg" alt=""></div>
-						<div class="product_content">
-							<div class="product_title"><a href="product.html">Smart Phone</a></div>
-							<div class="product_price">$710</div>
-						</div>
-					</div> -->
-
-					<!-- Product -->
-					<!-- <div class="product">
-						<div class="product_image"><img src="/template/images/product_4.jpg" alt=""></div>
-						<div class="product_content">
-							<div class="product_title"><a href="product.html">Smart Phone</a></div>
-							<div class="product_price">$330</div>
-						</div>
-					</div> -->
-
-
 				</div>
 			</div>
 		</div>
--- views/site/index.php
+++ views/site/index.php
@@ -127,16 +127,6 @@
 				<div class="col">
 					
 					<div class="product_grid">
-
-						<!-- Product -->
-						<!-- <div class="product">
-							
-							<div class="product_extra product_new"><a href="categories.html">New</a></div>
-							<div class="product_content">
-								<div class="product_title"><a href="product.html">Smart Phone</a></div>
-								<div class="product_price">$670</div>
-							</div>
-						</div> -->
 						
 						<?php foreach ($latestProducts as $product): ?>
                         	<div class="product">
@@ -155,72 +145,6 @@
                             </div>
                         	</div>
                     	<?php endforeach; ?>
-
-						<!-- Product -->
-						<!-- <div class="product">
-							<div class="product_image"><img src="/template/images/product_2.jpg" alt=""></div>
-							<div class="product_extra product_sale"><a href="categories.html">Sale</a></div>
-							<div class="product_content">
-								<div class="product_title"><a href="product.html">Smart Phone</a></div>
-								<div class="product_price">$670</div>
-							</div>
-						</div> -->
-
-						<!-- Product -->
-						<!-- <div class="product">
-							<div class="product_image"><img src="/template/images/product_3.jpg" alt=""></div>
-							<div class="product_content">
-								<div class="product_title"><a href="product.html">Smart Phone</a></div>
-								<div class="product_price">$670</div>
-							</div>
-						</div> -->
-
-						<!-- Product -->
-						<!-- <div class="product">
-							<div class="product_image"><img src="/template/images/product_4.jpg" alt=""></div>
-							<div class="product_content">
-								<div class="product_title"><a href="product.html">Smart Phone</a></div>
-								<div class="product_price">$670</div>
-							</div>
-						</div> -->
-
-						<!-- Product -->
-						<!-- <div class="product">
-							<div class="product_image"><img src="/template/images/product_5.jpg" alt=""></div>
-							<div class="product_content">
-								<div class="product_title"><a href="product.html">Smart Phone</a></div>
-								<div class="product_price">$670</div>
-							</div>
-						</div> -->
-
-						<!-- Product -->
-						<!-- <div class="product">
-							<div class="product_image"><img src="/template/images/product_6.jpg" alt=""></div>
-							<div class="product_extra product_hot"><a href="categories.html">Hot</a></div>
-							<div class="product_content">
-								<div class="product_title"><a href="product.html">Smart Phone</a></div>
-								<div class="product_price">$670</div>
-							</div>
-						</div> -->
-
-						<!-- Product -->
-						<!-- <div class="product">
-							<div class="product_image"><img src="/template/images/product_7.jpg" alt=""></div>
-							<div class="product_content">
-								<div class="product_title"><a href="product.html">Smart Phone</a></div>
-								<div class="product_price">$670</div>
-							</div>
-						</div> -->
-
-						<!-- Product -->
-						<!-- <div class="product">
-							<div class="product_image"><img src="/template/images/product_8.jpg" alt=""></div>
-							<div class="product_extra product_sale"><a href="categories.html">Hot</a></div>
-							<div class="product_content">
-								<div class="product_title"><a href="product.html">Smart Phone</a></div>
-								<div class="product_price">$670</div>
-							</div>
-						</div> -->
 
 					</div>						
 				</div>
